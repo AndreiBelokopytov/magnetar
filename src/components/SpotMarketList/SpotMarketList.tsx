47cdcdfc3ad3418dbce9918a5d8b006a8cc53f96
Andrei Belokopytov <belokopytov87@gmail.com>
2021-11-10 21:04:06 +0300

    Improve SpotMarketList scroll

--- src/components/SpotMarketList/SpotMarketList.tsx
+++ src/components/SpotMarketList/SpotMarketList.tsx
@@ -1,14 +1,16 @@
 import { observer } from "mobx-react";
 import { SpotMarketListItem, SpotMarketListItemVM } from "./SpotMarketListItem";
-import { FlatList, ListRenderItemInfo } from "react-native";
 
 type Props = {
   items: SpotMarketListItemVM[];
 };
 
 export const SpotMarketList = observer(({ items }: Props) => {
-  const renderItem = (item: ListRenderItemInfo<SpotMarketListItemVM>) => <SpotMarketListItem model={item.item} />;
-  const keyExtractor = (model: SpotMarketListItemVM) => model.pair;
-
-  return <FlatList renderItem={renderItem} data={items} keyExtractor={keyExtractor} />;
+  return (
+    <>
+      {items.map((item) => (
+        <SpotMarketListItem model={item} key={item.pair} />
+      ))}
+    </>
+  );
 });
