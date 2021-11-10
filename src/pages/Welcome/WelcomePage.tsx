19b0bc6d484688f5323536da3393721841833689
Andrei Belokopytov <belokopytov87@gmail.com>
2021-11-10 20:24:00 +0300

    Update WelcomePage

--- src/pages/Welcome/WelcomePage.tsx
+++ src/pages/Welcome/WelcomePage.tsx
@@ -1,14 +1,28 @@
-import { SpotMarketList } from "../../components";
+import { SpotMarketList, StackView } from "../../components";
 import { useDependency } from "../../DIContainer";
 import { SpotMarketAdapter } from "./adapters";
 import React from "react";
+import { observer } from "mobx-react";
+import { StyleSheet } from "react-native";
 
-export const WelcomePage = () => {
+export const WelcomePage = observer(() => {
   const adapter = useDependency<SpotMarketAdapter>(SpotMarketAdapter);
 
   React.useEffect(() => {
     adapter?.refresh();
   }, [adapter]);
 
-  return <SpotMarketList items={adapter?.marketListItems ?? []} />;
-};
+  return (
+    <StackView alignItems={"center"} height={"100%"}>
+      <StackView flex width={600} style={styles.content}>
+        <SpotMarketList items={adapter?.marketListItems ?? []} />
+      </StackView>
+    </StackView>
+  );
+});
+
+const styles = StyleSheet.create({
+  content: {
+    overflow: "visible",
+  },
+});
