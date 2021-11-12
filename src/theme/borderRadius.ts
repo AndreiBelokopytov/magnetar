46753427fa7ec510128559844125268e075c6d8a
Andrei Belokopytov <belokopytov87@gmail.com>
2021-11-12 14:04:04 +0300

    Implement application theme

---
+++ src/theme/borderRadius.ts
@@ -0,0 +1,6 @@
+export const borderRadius = {
+  base: 4,
+  get button() {
+    return this.base;
+  },
+};
---
+++ src/theme/colors.ts
@@ -0,0 +1,6 @@
+export const colors = {
+  white: "#fff",
+  black: "#000",
+  primary: "#841fd6",
+  secondary: "#D90368",
+};
