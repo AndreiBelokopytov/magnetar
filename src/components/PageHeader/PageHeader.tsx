d321b989436ef5e948885a4929c16e5f8a23c487
Andrei Belokopytov <belokopytov87@gmail.com>
2021-11-12 14:30:39 +0300

    Update PageHeader

--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -1,20 +1,28 @@
-import React from "react";
-import { Spacer, StackView, Typography, Button } from "~/components";
+import React, { useState } from "react";
+import { Spacer, StackView, Typography, Button, WalletSelectionModal } from "~/components";
 
 export const PageHeader = () => {
+  const [isWalletSelectionOpen, setIsWalletSelectionOpen] = useState(false);
+
+  const openWalletModal = () => setIsWalletSelectionOpen(true);
+  const closeWalletModal = () => setIsWalletSelectionOpen(false);
+
   return (
-    <StackView
-      direction={"row"}
-      width={"100%"}
-      height={60}
-      alignItems={"center"}
-      pl={32}
-      pr={32}
-      bgColor={"ghostwhite"}
-    >
-      <Typography variant={"h3"}>Magnetar</Typography>
-      <Spacer />
-      <Button title={"Connect wallet"} />
-    </StackView>
+    <>
+      <StackView
+        direction={"row"}
+        width={"100%"}
+        height={60}
+        alignItems={"center"}
+        pl={32}
+        pr={32}
+        bgColor={"ghostwhite"}
+      >
+        <Typography variant={"h3"}>Magnetar</Typography>
+        <Spacer />
+        <Button onPress={openWalletModal}>Connect wallet</Button>
+      </StackView>
+      <WalletSelectionModal open={isWalletSelectionOpen} onClose={closeWalletModal} />
+    </>
   );
 };
