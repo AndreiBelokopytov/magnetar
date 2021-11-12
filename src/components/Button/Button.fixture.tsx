a73b63bbcef8337ea87019ec94d6b5325a387652
Andrei Belokopytov <belokopytov87@gmail.com>
2021-11-12 14:04:50 +0300

    Refactor Button

--- src/components/Button/Button.fixture.tsx
+++ src/components/Button/Button.fixture.tsx
@@ -1,3 +1,3 @@
 import { Button } from "./Button";
 
-export default <Button title={"Click me!"} />;
+export default <Button>Click me</Button>;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,34 @@
-import { Button as RNButton, ButtonProps } from "react-native";
+import { TouchableOpacity, TouchableOpacityProps, ViewStyle, StyleSheet } from "react-native";
+import { StackView, Typography } from "~/components";
+import { borderRadius, colors } from "~/theme";
 
-type Props = ButtonProps;
+type Props = {
+  color?: string;
+  fontColor?: string;
+  style?: ViewStyle;
+} & Omit<TouchableOpacityProps, "style">;
 
-export const Button = (props: Props) => {
-  return <RNButton {...props} />;
+export const Button = ({ color, fontColor, style, children, ...rest }: Props) => {
+  return (
+    <TouchableOpacity {...rest}>
+      <StackView
+        direction={"row"}
+        pl={24}
+        pr={24}
+        pt={8}
+        pb={8}
+        style={style}
+        bgColor={colors.primary}
+        borderRadius={borderRadius.button}
+      >
+        <Typography style={styles.primary}>{children}</Typography>
+      </StackView>
+    </TouchableOpacity>
+  );
 };
+
+const styles = StyleSheet.create({
+  primary: {
+    color: colors.white,
+  },
+});
