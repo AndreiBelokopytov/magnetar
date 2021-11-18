e24d3a6e276527224e65dd3d0c28bb97f655f3c2
Andrei Belokopytov <belokopytov87@gmail.com>
2021-11-18 22:17:30 +0300

    Fix bug in SportMarketListContainer

--- src/pages/_containers/SportMarketListContainer.tsx
+++ src/pages/_containers/SportMarketListContainer.tsx
@@ -4,11 +4,12 @@ import React from "react";
 import { Disposer } from "~/utils";
 import { SpotMarketList, SpotMarketListItemVM } from "~/components";
 import { useIntervalRefresh } from "~/hooks";
+import { observer } from "mobx-react";
 
 const DEFAULT_MARKET_LIST_ITEMS: SpotMarketListItemVM[] = [];
 const REFRESH_INTERVAL = 3000;
 
-export const SportMarketListContainer = () => {
+export const SportMarketListContainer = observer(() => {
   const spotMarketAdapter = useInstanceOf<SpotMarketAdapter>(SpotMarketAdapter);
 
   React.useEffect(() => {
@@ -26,4 +27,4 @@ export const SportMarketListContainer = () => {
       items={spotMarketAdapter?.marketListItems ?? DEFAULT_MARKET_LIST_ITEMS}
     />
   );
-};
+});
