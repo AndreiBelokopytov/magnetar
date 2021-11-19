e85af74d3a427bd7a7e5bd31e5a036e43892a5fc
Andrei Belokopytov <belokopytov87@gmail.com>
2021-11-19 11:59:57 +0300

    Implement DerivativesMarketStore

--- src/stores/stores.module.ts
+++ src/stores/stores.module.ts
@@ -3,8 +3,11 @@ import { SpotMarketStore } from "./SpotMarketStore";
 import { SpotMarketStoreImpl } from "./SpotMarketStore.impl";
 import { WalletSettingsStore } from "./WalletSettingsStore";
 import { WalletSettingsStoreImpl } from "./WalletSettingsStore.impl";
+import { DerivativesMarketStore } from "./DerivativesMarketStore";
+import { DerivativesMarketStoreImpl } from "./DerivativesMarketStore.impl";
 
 export const storesModule = new ContainerModule((bind: interfaces.Bind) => {
   bind<SpotMarketStore>(SpotMarketStore).to(SpotMarketStoreImpl);
+  bind<DerivativesMarketStore>(DerivativesMarketStore).to(DerivativesMarketStoreImpl);
   bind<WalletSettingsStore>(WalletSettingsStore).to(WalletSettingsStoreImpl);
 });
