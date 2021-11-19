0d20bc0cf485e839aadd87a492ce2c733958a43f
Andrei Belokopytov <belokopytov87@gmail.com>
2021-11-19 12:07:16 +0300

    Implement routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,22 @@ import "reflect-metadata";
 import { AppRegistry } from "react-native";
 import { DIContainer } from "./DIContainer";
 import { WelcomePage } from "./pages";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
       <DIContainer>
+        <Router>
+          <Switch>
+            <Route path={"/"}>
+              <Redirect to={"markets"} />
+            </Route>
+            <Route path={"/markets"}>
+              <WelcomePage />
+            </Route>
+          </Switch>
+        </Router>
         <WelcomePage />
       </DIContainer>
     );
