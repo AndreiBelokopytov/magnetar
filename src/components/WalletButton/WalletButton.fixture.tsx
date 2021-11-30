1aadf52258c808dda52c5c6343f3bc433f97f028
Vadim Buldenko <vadimir33@gmail.com>
2021-11-30 21:25:32 +0400

    issue#3/add-disabled-atribute-for-WalletButton (готово)

--- src/components/WalletButton/WalletButton.fixture.tsx
+++ src/components/WalletButton/WalletButton.fixture.tsx
@@ -2,10 +2,28 @@ import { Fixture } from "~/components/_utils";
 import { WalletButton } from "~/components";
 import { Text } from "grommet";
 
-export default (
+const primary = () => (
   <Fixture>
     <WalletButton>
       <Text>It's a Button</Text>
     </WalletButton>
   </Fixture>
 );
+
+const disabled = () => (
+  <Fixture>
+    <WalletButton disabled>
+      <Text>It's a disabled Button</Text>
+    </WalletButton>
+  </Fixture>
+);
+
+const loading = () => (
+  <Fixture>
+    <WalletButton loading>
+      <Text>It's a loading Button</Text>
+    </WalletButton>
+  </Fixture>
+);
+
+export default { primary, disabled, loading };
