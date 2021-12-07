1b2baa769048bb66041e38602f66448ec6568601
Andrei Belokopytov <belokopytov87@gmail.com>
2021-12-07 18:17:14 +0300

    Fix page footer

--- src/components/PageFooter/PageFooter.tsx
+++ src/components/PageFooter/PageFooter.tsx
@@ -2,21 +2,15 @@ import { Box, Text } from "grommet";
 import { GradientText } from "./GradientText";
 
 export const PageFooter = () => {
-    return (
-        <Box direction="row" height="100%" align="center">
-            <Box flex={{grow: 1}}>
-                <Text size="small" weight={500}>
-                    © 2021 Open DeFi Foundation
-                </Text>
-            </Box>
-            <Box>
-                <Text size="small" weight={500}>
-                    <GradientText>
-                        powered by Injective
-                    </GradientText>
-                </Text>
-            </Box>
-            <Box flex={{grow: 1}}></Box>
-        </Box>
-    );
-}
+  return (
+    <Box direction="row" height="100%" align="center">
+      <Box flex={{ grow: 1 }} />
+      <Box>
+        <Text size="small" weight={500}>
+          <GradientText>powered by Injective</GradientText>
+        </Text>
+      </Box>
+      <Box flex={{ grow: 1 }} />
+    </Box>
+  );
+};
